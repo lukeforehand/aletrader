cb2cfe50c3810a76158f061cf5de10b3f38f1979
lukeforehand <lukeforehand@gmail.com>
2014-02-23 11:27:51 -0600

    using scala

--- src/main/java/com/aletrader/Luke.java
+++
@@ -1,8 +0,0 @@
-package org.luke;
-
-public class Luke {
-	public static void main(String[] args) {
-		System.out.println("hello world");
-	}
-	
-}
---
+++ src/main/scala/com/aletrader/Luke.scala
@@ -0,0 +1,5 @@
+object Luke {
+  def main(args: Array[String]) {
+    println("Hello, world!")
+  }
+}
