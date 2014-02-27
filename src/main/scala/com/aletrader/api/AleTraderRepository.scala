7762d16e1b8288738ee726e2ad7ae35a790cf45d
lukeforehand <lukeforehand@gmail.com>
2014-02-26 22:07:14 -0600

    converted unit tests to scala, attempting to add spring component scanning and jersey-spring servlet

--- src/main/scala/com/aletrader/api/AleTraderRepository.scala
+++ src/main/scala/com/aletrader/api/AleTraderRepository.scala
@@ -1,24 +1,37 @@
 
 package com.aletrader.api;
 
+import com.aletrader.cellar.label.LabelGenerator;
+
 import java.util.ResourceBundle;
 
 import org.elasticsearch.client.Client;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
 import org.elasticsearch.action.admin.cluster.state.ClusterStateResponse;
+import org.elasticsearch.node.Node;
+import org.elasticsearch.node.NodeBuilder._;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Component;
 
 // this class will eventually become a facade to accessing elasticsearch and postgresql, i.e. data access
-class AleTraderRepository(client: Client) {
+@Component
+class AleTraderRepository(node: Node) {
 
-	def this() = this(new TransportClient().addTransportAddress(new InetSocketTransportAddress(ResourceBundle.getBundle("application").getString("es.host"), 9300)));
+	// elasticsearch client
+	var es = node.client();
 
-	def destroy() {
-		client.close();
-	}
+	// label generator
+	var labels = LabelGenerator;
+
+	//TODO: add dao layer --> need to choose technology
+
+	def this() = this(nodeBuilder().client(true).build());
 
 	def clusterState(): ClusterStateResponse = {
-		return client.admin().cluster().prepareState().execute().actionGet();
+		return es.admin().cluster().prepareState().execute().actionGet();
 	}
 
 }
