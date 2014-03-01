b5be5a52fc47a4c8e3170090b8f686cf2967e17b
Luke Forehand <lukeforehand@gmail.com>
2014-03-01 00:06:10 -0600

    Update Breweries.scala

--- src/main/scala/com/aletrader/brewerydb/Breweries.scala
+++ src/main/scala/com/aletrader/brewerydb/Breweries.scala
@@ -6,24 +6,7 @@ package com.aletrader.brewerydb;
   "status" : "success",
   "numberOfPages" : 63,
   "data" : [
-    {
-      "id" : "KlSsWY",
-      "description" : "",
-      "name" : "'t Hofbrouwerijke",
-      "createDate" : "2012-01-02 11:50:52",
-      "mailingListUrl" : "",
-      "updateDate" : "",
-      "images" : {
-        "medium" : "",
-        "large" : "",
-        "icon" : ""
-      },
-      "established" : "",
-      "isOrganic" : "N",
-      "website" : "http://www.thofbrouwerijke.be/",
-      "status" : "verified",
-      "statusDisplay" : "Verified"
-    },
+    {...brewery...},
     ...
   ],
   "currentPage" : 1,
