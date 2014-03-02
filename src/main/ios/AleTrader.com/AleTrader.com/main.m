d9641b33c2022ca7cb482cc77b09b811e0878a62
lukeforehand <lukeforehand@gmail.com>
2014-03-02 15:39:33 -0600

    ios src

---
+++ src/main/ios/AleTrader.com/AleTrader.com/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AleTrader.com
+//
+//  Created by Lindsay Berger on 3/2/14.
+//  Copyright (c) 2014 Luke Forehand. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ src/main/ios/AleTrader.com/AleTrader.comTests/AleTrader_comTests.m
@@ -0,0 +1,34 @@
+//
+//  AleTrader_comTests.m
+//  AleTrader.comTests
+//
+//  Created by Lindsay Berger on 3/2/14.
+//  Copyright (c) 2014 Luke Forehand. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AleTrader_comTests : XCTestCase
+
+@end
+
+@implementation AleTrader_comTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
