1e9a78095183d8a3f538ab5ed209271495280adb
Nate Schneider <nschneider@nis-MacBook-Pro.local>
2014-03-01 22:48:03 -0600

    Adding initial cellar item management skeleton code

--- src/main/scala/com/aletrader/api/AleTraderRepository.scala
+++ src/main/scala/com/aletrader/api/AleTraderRepository.scala
@@ -10,6 +10,7 @@ import org.elasticsearch.action.admin.cluster.state.ClusterStateResponse;
 import org.elasticsearch.node.Node;
 import org.elasticsearch.node.NodeBuilder._;
 
+import com.aletrader.api.model.CellarItem;
 import com.aletrader.brewerydb._;
 
 // this class will eventually become a facade to accessing elasticsearch and postgresql, i.e. data access
@@ -23,6 +24,45 @@ class AleTraderRepository(node: Node) {
 	//TODO: add dao layer --> need to choose technology
 
 	def this() = this(nodeBuilder().client(true).build());
+	
+	
+	//
+	// Find Beer
+	
+	def searchUpc(code: String): Beers = {
+		return breweryDbClient.searchUpc(code);
+	}
+	
+	def searchBeers(query: String): Beers = {
+		// TODO: get Beers from ES based on user query
+		return null;
+	}
+	
+	
+	//
+	// Cellar Item Management
+	
+	def getCellarItem(cellarItemId: String): CellarItem = {
+		// TODO: get Cellar Item from ES
+		return new CellarItem("abc123", "Test");
+	}
+	
+	def addCellarItem(cellarItem: CellarItem) {
+		// TODO add CellarItem to ES
+	}
+	
+	def updateCellarItem(cellarItem: CellarItem) {
+		// TODO update CellarItem to ES
+	}
+	
+	def removeCellarItem(cellarItemId: String) {
+		// TODO update CellarItem to no longer be in the current cellar
+	}
+	
+	
+	
+	// 
+	// Verification
 
 	def verifyElasticsearch(): ClusterStateResponse = {
 		return esClient.admin().cluster().prepareState().execute().actionGet();
@@ -32,9 +72,6 @@ class AleTraderRepository(node: Node) {
 		return breweryDbClient.verifyKey();
 	}
 
-	// first actual repo method
-	def searchUpc(code: String): Beers = {
-		return breweryDbClient.searchUpc(code);
-	}
+
 
 }
